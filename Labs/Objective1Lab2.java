7390fb6978df1393002d0e441a21492a0b885104
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-07-05 12:39:44 -0600

    Objective 1 Complete

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2 {
+  public static void main(String[] args ) {
+  System.out.println(" My name is Gabriel Antonia.");
+  System.out.println(" I am 31 years old.");
+  }
+}
