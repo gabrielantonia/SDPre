e1c859b37df8b67f98cfbc027d4676d2f93fe376
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-07-06 16:13:22 -0600

    Exercise 21 Completed

---
+++ JavaTheHardWay/GenderTitles.java
@@ -0,0 +1,38 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class GenderTitles{
+  public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    String title;
+
+    out.print("First name: ");
+    String first = keyboard.next();
+    out.print("Last name: ");
+    String last = keyboard.next();
+    out.print("Gender (M/F): ");
+    String gender = keyboard.next();
+    out.print("Age: ");
+    int age = keyboard.nextInt();
+
+    if (age < 20){
+      title = first;
+    }
+    else {
+      if (gender.equals("F")){
+        System.out.print(" Are you married, "+first+"? (Y/N): ");
+        String married = keyboard.next();
+        if(married.equals("Y")){
+          title = "Mrs.";
+        }
+        else{
+          title ="Ms.";
+        }
+      }
+      else{
+        title = "Mr.";
+      }
+    }
+    out.println("\n" +title+ " "+last);
+  }
+}
