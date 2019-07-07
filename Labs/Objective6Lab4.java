2ae8645809625de79f572fdcd055fc8acb1cabd8
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-07-07 14:09:47 -0600

    Objective 6 Completed

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count=0, sum=0;
+
+    while(count<20){
+      count++;
+      sum=sum+count;
+      //System.out.println(sum);
+      //If you want to see each sum
+    }
+    //If you want to only see total sum
+    System.out.println(sum);
+  }
+}
